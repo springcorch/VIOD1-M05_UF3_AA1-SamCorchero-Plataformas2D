90987a15d0b492af51eeac42e45e7ba4352544f2
springcorch <sam.corchero@enti.cat>
2025-05-11 21:50:24 +0200

    [ 4 ] - PROTOTYPE: DoubleJump and Platform commit
    - Edit Script PlayerMovement: Se ha añadido un doble salto que se ejecuta al estar en el aire y pulsar espacio, solo se puede hacer uno, por lo tanto, al realizar un doble salto, pasa a estar a false hasta que vuelva a tocar el suelo.
    
    - New GameObject Platform, con collider2D controlado por un effector2D, este effector2D sirve para que al tocar desde la parte superior, el collider actua normal, pero por la parte de abajo es transparente, lo cual al saltar, por abajo se traspasará pero por arriba se quedará.
    
    - New Script Platform: Cogemos el componente de effector2D de nuestro gameObject y le decimos que al darle espacio, el collider será afectado cuando llegue el Player por arriba, lo que hará que se quede en la plataforma, pero al querer bajar, en un delay de 0.5 segundos, el effector hará una rotación de 180 grados, lo que significa que el collider afectará por abajo, haciendo caer al Player y se reseteará al volver a darle al espacio.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //Variables para el movimiento (move)
-    public float moveSpeed = 2;
-    //Animaciones para movimiento
-    private SpriteRenderer spriteRenderer;
-    private Animator animator;
+    public float moveSpeed = 2f;
 
     //Variables para el movimiento (jump)
-    public float jumpSpeed = 3;
+    public float jumpSpeed = 3f;
+    public float doubleJumpSpeed = 2.5f;
+
+    private bool canDoubleJump;
 
     //Multiplicadores para que depende como cliquemos el espacio vaya mas alto o mas bajo
     public float fallMultiplier = 0.5f;
@@ -17,6 +17,9 @@ public class PlayerMovement : MonoBehaviour
 
     //El rigidbody de nuestro personaje
     private Rigidbody2D rb;
+    //Animaciones para movimiento
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
     void Start() {
         //Busca los componentes en el objeto:
@@ -24,18 +27,70 @@ public class PlayerMovement : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
+    
+    //Para el jump, hace mejor las comprobaciones
+    private void Update() {
+        //Movimiento vertical del personaje (cae por gravedad del rb):
+        if (Input.GetKey("space"))
+        {
+            //Si toca el suelo - if
+            if (CheckGround.isGrounded)
+            {
+                canDoubleJump = true;
+                rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            }
+            else
+            {
+                //Si pulsamos otra vez space en el aire
+                if (Input.GetKeyDown("space"))
+                {
+                    //Haremos doble salto y volveremos a no poder saltar otra vez
+                    if (canDoubleJump)
+                    {
+                        animator.SetBool("DoubleJump", true);
+                        rb.velocity = new Vector2(rb.velocity.x, doubleJumpSpeed);
+                        canDoubleJump = false;
+                    }
+                }
+            }
+        }
+
+        //Logica de animacion de salto
+        if (!CheckGround.isGrounded)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+        }
+        if (CheckGround.isGrounded)
+        {
+            animator.SetBool("Jump", false);
+            animator.SetBool("DoubleJump", false);
+            animator.SetBool("Falling", true);
+        }
+
+        //Si estamos cayendo (y < 0)
+        if (rb.velocity.y < 0)
+        {
+            animator.SetBool("Falling", true);
+        }
+        //Si estamos subiendo (y > 0)
+        else if (rb.velocity.y > 0)
+        {
+            animator.SetBool("Falling", false);
+        }
+    }
 
     void FixedUpdate() {
         //Movimiento horizontal del personaje:
         //Derecha
-        if (Input.GetKey("d") || Input.GetKey("right"))
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
         {
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
             spriteRenderer.flipX = false;
             animator.SetBool("Run", true);
         }
         //Izquierda
-        else if (Input.GetKey("a") || Input.GetKey("left"))
+        else if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
             spriteRenderer.flipX = true;
@@ -45,22 +100,6 @@ public class PlayerMovement : MonoBehaviour
         else { 
             rb.velocity = new Vector2(0, rb.velocity.y);
             animator.SetBool("Run", false);
-        }
-
-        //Movimiento vertical del personaje (cae por gravedad del rb):
-        if (Input.GetKey("space") && CheckGround.isGrounded)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-        }
-
-        if (!CheckGround.isGrounded)
-        {
-            animator.SetBool("Jump", true);
-            animator.SetBool("Run", false);
-        }
-        if (CheckGround.isGrounded) 
-        {
-            animator.SetBool("Jump", false);
         }
 
         //Mejorar el salto con las fisicas de la gravedad y el Vector.up que hace que solo afecte al +y
