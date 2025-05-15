5f7490193860ebdcaa0a49323e45b59b97ec40d3
springcorch <sam.corchero@enti.cat>
2025-05-15 13:47:28 +0200

    [ 18 ] - PROTOTYPE: Wall Sliding commit
    - New gameObject walls que contiene todos esos colliders que queremos que se pueda deslizar el personaje
    
    - Edit script Playermovemnt: añadimos que mientras se colisione el jugador con una pared, este se desliza hacia abajo de forma más lenta sin poder saltar (por ahora).

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,6 +11,14 @@ public class PlayerMovement : MonoBehaviour
 
     private bool canDoubleJump;
 
+    //Variables para el movimiento (wall)
+    public float wallSlidingSpeed = 1.25f;
+    private bool isTouchingWall = false;
+    private bool wallSliding;
+
+    private bool rightWall;
+    private bool leftWall;
+
     //El rigidbody de nuestro personaje
     private Rigidbody2D rb;
     //Animaciones para movimiento
@@ -27,7 +35,7 @@ public class PlayerMovement : MonoBehaviour
     //Para el jump, hace mejor las comprobaciones
     private void Update() {
         //Movimiento vertical del personaje (cae por gravedad del rb):
-        if (Input.GetKey("space"))
+        if (Input.GetKey("space") && !wallSliding)
         {
             //Si toca el suelo - if
             if (CheckGround.isGrounded)
@@ -74,19 +82,35 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetBool("Falling", false);
         }
+
+        //WALL SLIDING LOGIC
+        if (isTouchingWall && !CheckGround.isGrounded) { 
+            wallSliding = true;
+        }
+        else
+        {
+            wallSliding = false;
+        }
+
+        if (wallSliding) {
+            animator.Play("Wall");
+            //Mathf.Clamp se utiliza para tener un movimiento constante y que no se note la diferencia con MaxValue
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+
     }
 
     void FixedUpdate() {
         //Movimiento horizontal del personaje:
         //Derecha
-        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow) && !rightWall)
         {
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
             spriteRenderer.flipX = false;
             animator.SetBool("Run", true);
         }
         //Izquierda
-        else if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow) && !leftWall)
         {
             rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
             spriteRenderer.flipX = true;
@@ -97,5 +121,27 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(0, rb.velocity.y);
             animator.SetBool("Run", false);
         }
+    }
+
+    //Mientras este en colision...
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("RightWall"))
+        {
+            isTouchingWall = true;
+            rightWall = true;
+        }
+        if (collision.gameObject.CompareTag("LeftWall"))
+        {
+            isTouchingWall = true;
+            leftWall = true;
+        }
+    }
+    //Cuando se salga de la colision...
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isTouchingWall = false;
+        leftWall = false;
+        rightWall = false;
     }
 }
